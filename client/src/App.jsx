7906e72182525d288ae2791d17e1f070e9495952
Yasin <yasinadan2001@gmail.com>
2023-04-08 15:55:52 -0500

    navbar implemented.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,49 @@
 import {
   createBrowserRouter,
   RouterProvider,
+  Outlet,
 } from "react-router-dom";
 
 import Home from "./Components/Home/Home";
 import Products from "./Components/Products/Products";
 import Product from "./Components/Product/Product";
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
+
+import "./App.scss";
+
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+        <Outlet />
+      <Footer />
+    </div>
+  );
+}
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "products/:id",
+        element: <Products />,
+      },
+      {
+        path: "product/:id",
+        element: <Product />,
+      }
+    ]
   },
-  {
-    path: "products/:id",
-    element: <Products />,
-  },
-  {
-    path: "product/:id",
-    element: <Product />,
-  }
 ]);
+
 
 function App() {
 
